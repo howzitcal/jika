07415e932af948e8e386dc1e756145118ccf16c7
Callum Fleming <mrcallumfleming@gmail.com>
2025-10-29 07:12:37 +0000

    update script

--- 24.04.sh
+++ 24.04.sh
@@ -103,78 +103,24 @@ dconf write /org/gnome/shell/extensions/tophat/cpu-sort-cores false
 dconf write /org/gnome/shell/extensions/tophat/mem-display "'both'"
 dconf write /org/gnome/shell/extensions/tophat/position-in-panel "'left'"
 
+# remove app folders
+gsettings set org.gnome.desktop.app-folders folder-children "['YaST']"
 
 # remove un-used snaps
 sudo snap remove --no-wait snap-store firefox
 
-sudo apt-get install -yq papirus-icon-theme gnome-software-plugin-snap gnome-software-plugin-flatpak gnome-shell-extension-manager vlc tilix unzip breeze-cursor-theme
-sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+# apt installs
+sudo apt-get install -yq papirus-icon-theme gnome-software-plugin-snap gnome-software-plugin-flatpak gnome-shell-extension-manager vlc tilix unzip breeze-cursor-theme ca-certificates wget gpg gnupg2 apt-transport-https gnome-screenshot
 
-dconf write /com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/background-transparency-percent 4
-dconf write /com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/login-shell true
-
+# apt removes
 sudo apt-get -yq remove gnome-terminal
 
-if [[ -v dev ]]; then
-    echo "[INFO] installing dev tools"
+# add flathub
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-    # dbeaver
-    wget -c https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb -O $DOWNLOAD_PATH/dbeaver.deb
-    sudo apt install -yq $DOWNLOAD_PATH/dbeaver.deb
-
-    # docker
-    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-    sudo apt-get update
-    sudo apt-get install -yq ca-certificates curl
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
-    echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt update
-    sudo apt-get -yq install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-    sudo usermod -aG docker $USER
-
-    # code
-    echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
-    sudo apt-get install wget gpg
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-    sudo install -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg
-    rm -f microsoft.gpg
-sudo tee /etc/apt/sources.list.d/vscode.sources > /dev/null <<'EOF'
-Types: deb
-URIs: https://packages.microsoft.com/repos/code
-Suites: stable
-Components: main
-Architectures: amd64,arm64,armhf
-Signed-By: /usr/share/keyrings/microsoft.gpg
-EOF
-    sudo apt-get install -yq apt-transport-https
-    sudo apt update
-    sudo apt-get install -yq code
-fi
-
-if [[ -v chrome ]]; then
-    echo "[INFO] installing chrome"
-
-    wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O $DOWNLOAD_PATH/chrome.deb
-    sudo apt install -yq $DOWNLOAD_PATH/chrome.deb
-fi
-
-ssh-keygen -f $HOME/.ssh/id_rsa -N ""
-
-if [[ -v git_username ]]; then
-    git config --global user.name "$git_username"
-fi
-
-if [[ -v git_email ]]; then
-    git config --global user.email "$git_email"
-fi
-
-echo "[INFO] Setting up theme..."
-
+# theme start
+dconf write /com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/background-transparency-percent 4
+dconf write /com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d/login-shell true
 
 mkdir $HOME/.themes
 wget https://github.com/imarkoff/Marble-shell-theme/releases/latest/download/Marble-shell-filled.zip -O $HOME/.themes/marble.zip
@@ -187,14 +133,74 @@ gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 dconf write /org/gnome/shell/extensions/user-theme/name "'Marble-blue-dark'"
 dconf write /org/gnome/desktop/interface/cursor-theme "'breeze_cursors'"
 
-# gsettings set org.gnome.desktop.interface accent-color 'blue'
-
-sudo snap refresh --no-wait
-
 mkdir -p $HOME/Pictures/Wallpapers
 wget https://raw.githubusercontent.com/howzitcal/jika/refs/heads/main/assets/wallpapers/simple-waves.png -O $HOME/Pictures/Wallpapers/simple-waves.png
 gsettings set org.gnome.desktop.background picture-uri-dark file://$HOME/Pictures/Wallpapers/simple-waves.png
+# theme end
 
+# shortcuts
+dconf write /org/gnome/shell/keybindings/show-screenshot-ui "['<Shift><Super>s']"
+
+if [[ -v dbeaver ]]; then
+    wget -c https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb -O $DOWNLOAD_PATH/dbeaver.deb
+    sudo apt install -yq $DOWNLOAD_PATH/dbeaver.deb
+fi
+
+if [[ -v docker ]]; then
+    for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update
+    sudo apt-get -yq install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo usermod -aG docker $USER
+fi
+
+if [[ -v vscode ]]; then
+    echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo install -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg
+    rm -f microsoft.gpg
+
+sudo tee /etc/apt/sources.list.d/vscode.sources > /dev/null <<'EOF'
+Types: deb
+URIs: https://packages.microsoft.com/repos/code
+Suites: stable
+Components: main
+Architectures: amd64,arm64,armhf
+Signed-By: /usr/share/keyrings/microsoft.gpg
+EOF
+
+    sudo apt update
+    sudo apt-get install -yq code
+fi
+
+if [[ -v chrome ]]; then
+    wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O $DOWNLOAD_PATH/chrome.deb
+    sudo apt install -yq $DOWNLOAD_PATH/chrome.deb
+fi
+
+if [[ -v brave ]]; then
+    curl -fsS https://dl.brave.com/install.sh | sh
+fi
+
+if [[ -v ssh_keygen ]]; then
+    ssh-keygen -f $HOME/.ssh/id_rsa -N ""
+fi
+
+if [[ -v git_username ]]; then
+    git config --global user.name "$git_username"
+fi
+
+if [[ -v git_email ]]; then
+    git config --global user.email "$git_email"
+fi
+
+# flatpaks are lower becuase they are slow to download(sometimes)
 sudo flatpak install  --noninteractive -y org.gtk.Gtk3theme.Adwaita-dark
 sudo flatpak override --env=GTK_THEME=Adwaita-dark
 
@@ -202,9 +208,16 @@ if [[ -v slack ]]; then
     flatpak install --noninteractive -y flathub com.slack.Slack
 fi
 
-gsettings set org.gnome.desktop.app-folders folder-children "['YaST']"
-
-
+if [[ -v kooha ]]; then
+    flatpak install --noninteractive -y io.github.seadve.Kooha
+    dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/binding "'<Shift><Super>r'"
+    dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/command "'flatpak run io.github.seadve.Kooha'"
+    dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/name "'kooha'"
+    dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+fi
 
 sudo apt autoremove -yq
 rm -rf $DOWNLOAD_PATH
+
+clear
+echo "Complete, please reboot your PC to finalize changes."
